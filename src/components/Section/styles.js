1eb7f6b4733f9b7772e2a5181763d1eb8ded8ec6
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-12 22:15:01 -0300

    criando componente de seção

---
+++ src/components/Section/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    padding: 24px 0 0 24px;
+
+    > h2 {
+        font-size: 18px;
+        font-weight: 500;
+    }
+`
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header"
 import { Footer } from "../../components/Footer"
+import { Section } from "../../components/Section"
 
 import { Container, Content, Flavors } from "./styles"
 
@@ -16,6 +17,10 @@ export function Home() {
                         <h2>Sabores inigualáveis</h2>
                         <p>Sinta o cuidado do preparo com ingredientes selecionados.</p>
                     </Flavors>
+
+                    <Section title="Refeição">
+                        
+                    </Section>
 
                 </div>
 
