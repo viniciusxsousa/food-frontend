368c674ea6542b232cc20d8167f46ea6e276c80b
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-14 20:15:27 -0300

    criando o componente input

---
+++ src/components/Input/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+    height: 48px;
+    border-radius: 8px;
+
+    background-color: ${({theme}) => theme.COLORS.DARK_900};
+`
