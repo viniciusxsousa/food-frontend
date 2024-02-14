c2294263ed37a5c1b63a7877193a6d1e5bc84f5b
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-14 20:36:49 -0300

    aplicando o padding no signin

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -4,8 +4,14 @@ export const Container = styled.div`
     width: 100%;
     height: 100vh;
 
+    padding: 0px 47px;
+
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+
+    > form {
+        width: 100%;
+    }
 `
