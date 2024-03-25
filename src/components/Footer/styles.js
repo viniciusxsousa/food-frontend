94090819ea86dcc13f622283458b9f3d2bfe9291
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-03-25 17:44:46 -0300

    ajustando o tamanho do texto no foorter em celulares grande

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { BREAKPOINTS } from '../../styles/breakpoints';
+
 export const Container = styled.footer`
     background-color: ${({theme}) => theme.COLORS.DARK_600};
     padding: 29px 20px;
@@ -9,19 +11,32 @@ export const Container = styled.footer`
     justify-content: center;
     gap: 6px;
 
+    @media screen and (min-width: ${BREAKPOINTS.MOBILEL}) {
+        gap: 2rem;
+    }
+
     > div {
         display: flex;
         align-items: center;
-        gap: 6px;
+        gap: 8px;
     }
 
     > div h2 {
         font-size: 1rem;
         font-weight: 500;
         color: ${({theme}) => theme.COLORS.LIGHT_700};
+
+        @media screen and (min-width: ${BREAKPOINTS.MOBILEL}){
+            font-size: 1.5rem;
+            font-weight: 700;
+        }
     }
 
     p {
         font-size: .9rem;
+
+        @media screen and (min-width: ${BREAKPOINTS.MOBILEL}){
+            font-size: 1.2rem;
+        }
     }
 `
