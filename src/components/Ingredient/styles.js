42815be8483d69f9bc33fe801d7ef986c80da4c9
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-04-02 20:00:17 -0300

    ajustando o tamanho do padding e font do componente ingredient

--- src/components/Ingredient/styles.js
+++ src/components/Ingredient/styles.js
@@ -4,6 +4,9 @@ export const Container = styled.span`
     background-color: ${({theme}) => theme.COLORS.DARK_1000};
     color: ${({theme}) => theme.COLORS.LIGHT_100};
 
-    padding: 8px 4px;
+    padding: 8px 10px;
     border-radius: 5px;
+
+    font-size: 14px;
+    font-weight: 500;
 `
