a1e989f6a6855b732ce177df3b8adb52bcc3754b
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-04-03 23:30:01 -0300

    aplicando a página de not found em todos conjuntos de rotas

--- src/routes/user.routes.jsx
+++ src/routes/user.routes.jsx
@@ -2,12 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
 import { Dished } from '../pages/Dished';
+import { NotFound } from '../pages/NotFound';
+
 
 export function UserRoutes() {
     return (
         <Routes>
             <Route path='/'  element={<Home/>} />
             <Route path='/dished/:id' element={<Dished/>} />
+
+            <Route path='*' element={<NotFound/>}/>
         </Routes>
     )
 }
